d3662f8df440b663bf5f903342c5ff176bf89362
lisamalaika <lisamalaika@gmail.com>
2020-01-26 15:02:49 +0100

    Add project file

---
+++ project_1.py
@@ -0,0 +1,28 @@
+# my new file
+var_1 = "Lisa"
+var_2 = "Jessica"
+var_3 = "Thomas"
+
+#print(var_1, var_2, var_3)
+# create array
+names = []
+# add variables to array
+names.append(var_1)
+names.append(var_2)
+names.append(var_3)
+names.append("Peter")
+# loop through array/ printing the variables
+for i in range(len(names)):
+    print(i + 1, names[i])
+
+#  https://stackoverflow.com/questions/29811082/how-to-print-out-a-numbered-list-in-python-3v
+
+# guessing game
+# create number in range
+# ask the question
+# get user input
+# evaluate
+# sucess message
+# error message
+# new guess
+# bonus certain amount of guesses
